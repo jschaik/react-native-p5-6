ec526eaa8566d10dac5a3667db35f845ee4e7316
jschaik <j.schaik@tcrmbo.nl>
2022-02-10 10:24:37 +0100

    welcomeScreen exercise 1

--- App.js
+++ App.js
@@ -1,34 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-
-  const pressHandler = () => {
-    console.log('pressed');
-  }
-  
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text} onPress={pressHandler}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" style={styles.bar}/>
-    </SafeAreaView>
+    <WelcomeScreen />
+      
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-      fontSize: 20,
-  },
-
-  bar: {
-    backgroundColor: 'black',
-  }
-});
