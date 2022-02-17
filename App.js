76f07153b95458ccbf7e4c731290b66fbfdae086
jschaik <j.schaik@tcrmbo.nl>
2022-02-17 09:55:53 +0100

    start styling

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
-    <WelcomeScreen />
-      
-    
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    <AppText>Hallo React Native</AppText>
+     </View>
   );
 }
---
+++ app/config/colors.js
@@ -0,0 +1,6 @@
+export default {
+  primary: "#eb4d4b",
+  secondary: "#0984e3",
+  black: "#000",
+  white: "#fff",
+};
